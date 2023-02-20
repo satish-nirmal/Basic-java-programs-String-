6b8a2584277cfa31a91899e5870b049949ea4a08
satish-nirmal <satishnirmal16@gmail.com>
2023-02-20 23:03:47 +0530

    This is for the practice purpose

---
+++ CountString.java
@@ -0,0 +1,21 @@
+public class Pattern{
+
+public static void main(String[] args)
+{
+
+for(int i=0;i<4;i++)
+{
+for(int j=0 ; j<4;j++){
+
+if(j-i<=0)
+{
+System.out.print("*");
+}
+else{
+System.out.print(" ");
+}
+}
+system.out.println();
+}
+}
+}
