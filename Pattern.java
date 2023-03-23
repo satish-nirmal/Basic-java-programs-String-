0e1eef6c38856a8dc7f74bb2cd7f519a4fe5e2af
satish-nirmal <satishnirmal16@gmail.com>
2023-03-24 01:08:32 +0530

    Thhis is the demo project

---
+++ Pattern.java
@@ -0,0 +1,21 @@
+public class Pattern{
+
+public static void main(String[] args)
+{
+
+for(int i=0;i<4;i++)
+{
+for(int j=0 ; j<4;j++){
+
+if(j+i<=3)
+{
+System.out.print("*");
+}
+else{
+System.out.print(" ");
+}
+}
+System.out.println();
+}
+}
+}
